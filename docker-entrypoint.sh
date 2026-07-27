57497be8261e188dfdd6c5b40c5cd451c65e9fbe
Amare Agerneh <amex44755@gmail.com>
2026-07-27 07:32:29 -0300

    Add environment variable checks before database tests

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -e
 
-echo "===== Testing database connection ====="
+echo "===== Shell Environment ====="
+env | grep database || true
 
+echo "===== PHP getenv() ====="
+php -r 'echo "HOST=" . getenv("database.default.hostname") . PHP_EOL;'
+php -r 'echo "DB=" . getenv("database.default.database") . PHP_EOL;'
+php -r 'echo "USER=" . getenv("database.default.username") . PHP_EOL;'
+php -r 'echo "PORT=" . getenv("database.default.port") . PHP_EOL;'
+
+echo "===== Testing database connection ====="
 php spark db:table
 
 echo "===== Running migrations ====="
-
 php spark migrate --all --no-interaction
 
 echo "===== Starting Apache ====="
-
 exec apache2-foreground
